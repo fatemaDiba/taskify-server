17dae63830c6df273d5ae55cdcbf73e7494c6f7e
Fatema Tabassum Diba <fatematabassum00700@gmail.com>
2025-02-23 23:56:07 +0600

    Added user route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,4 +2,33 @@ import express from "express";
 
 const userRouter = express.Router();
 
+//Get User
+userRouter.get("/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const result = await userCollection.findOne({ email });
+    return res.send(result);
+  } catch (err) {
+    console.log("Error Fetching User");
+    return res.status(500).send("Error Fetching User");
+  }
+});
+
+//Add User
+userRouter.post("/add", async (req, res) => {
+  const data = req.body;
+  try {
+    const existing = await userCollection.findOne({ email: data?.email });
+    if (existing) {
+      return res.send({ existing: true });
+    }
+
+    const result = await userCollection.insertOne({ ...data });
+    return res.send(result);
+  } catch (err) {
+    console.log("Error Adding User");
+    return res.status(500).send("Error Adding User");
+  }
+});
+
 export default userRouter;
